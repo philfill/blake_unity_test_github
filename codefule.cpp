8375f06d9edbf26085aa2983ccb95a6912e4d642
blake <blakemcclaren@gmail.com>
2019-11-21 23:54:46 -0800

    Making a conflict for Phil, lets see how he handles it!!! get'm phillfill.

---
+++ codefule.cpp
@@ -0,0 +1,27 @@
+#inlucde <iostream>
+
+int class codefule
+{
+public:
+	codefule(test);
+	~codefule();
+	
+
+};
+
+if (/* condition */)
+{
+	
+	cout << my face
+	for (int i = 0; i < count; ++i)
+	{
+		/* code */
+	}
+
+	/* code */
+}
+
+enum name
+{
+	this is an enum;	
+};
